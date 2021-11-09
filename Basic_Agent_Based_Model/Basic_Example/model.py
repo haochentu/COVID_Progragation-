1653bc971689368230e83f9f423dc1cfd90f2fd0
Haochen Tu <67667094+haochentu@users.noreply.github.com>
2021-11-09 18:07:20 +0000

    basic agent-based model
    this example is for the owner to understand the structure of the agent-based model in mesa

---
+++ Basic_Agent_Based_Model/Basic_Example/model.py
@@ -0,0 +1,24 @@
+from mesa import Agent, Model
+from .agent import MoneyAgent
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+
+class MoneyModel(Model):
+    """A model with some number of agents."""
+    def __init__(self, N, width, height):
+        self.num_agents = N
+        self.grid = MultiGrid(width,height, True)
+        self.schedule = RandomActivation(self)
+
+        # Create agents
+        for i in range(self.num_agents):
+            a = MoneyAgent(i, self)
+            self.schedule.add(a)
+            #Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a,(x,y))
+
+    def step(self):
+        '''Advance the model by one step'''
+        self.schedule.step()
---
+++ Basic_Agent_Based_Model/run.py
@@ -0,0 +1,3 @@
+from Basic_Example.server import server
+
+server.launch()
