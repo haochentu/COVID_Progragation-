cbaf278ecd152b49baeb8aa37e235f96761dfdff
Haochen Tu <67667094+haochentu@users.noreply.github.com>
2021-11-10 10:40:22 +0000

    Update agent.py

--- Basic_Agent_Based_Model/Basic_Example/agent.py
+++ Basic_Agent_Based_Model/Basic_Example/agent.py
@@ -6,26 +6,28 @@ class MoneyAgent(Agent):
         super().__init__(unique_id, model)
         self.wealth = 1
 
-    def step(self) -> None:
-        # the agent's step will go here
-        self.move()
-        if self.wealth > 0:
-            self.give_money()
+    def move(self):
+        possible_steps = self.model.grid.get_cell_list_contents(
+            self.pos,
+            moore = True,
+            include_center = False
+        )
+
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
 
     def give_money(self):
         cellmates = self.model.grid.get_cell_list_contents([self.pos])
         if len(cellmates) > 1:
             other = self.random.choice(cellmates)
-            other.wealth +=1
-            self.wealth -=1
+            other.wealth += 1
+            self.wealth -= 1
 
-    def move(self) -> None:
-        possible_steps = self.model.grid.get_cell_list_contents(
-            self.pos, moore = True, include_center = False
-        )
-
-        new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
+    def step(self):
+        # the agent's step will go here
+        self.move()
+        if self.wealth > 0:
+            self.give_money()
 
 
 
